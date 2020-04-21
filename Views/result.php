a1fbc837c751b068cc12ce336add03b1b0b04cdc
Arben Ferati <aslake36@gmail.com>
2020-04-21 22:02:00 +0200

    V.0.1

--- Views/result.php
+++ Views/result.php
@@ -8,11 +8,16 @@ $title="Resultat";
     <div class="card">
         <div class="card-body">
             <h4 class="card-title">Voici votre lien</h4>
-            <p>
-                <a href="https://golux.lausanne.ch/goeland/affaire2/specialisation/t274/genereate_png_position_arbres.php?npixwidth=300&npixheight=300&symbsize=8&strjson=[<?= $link ?>]">
-                    Votre image
-                </a>
-            </p>
+            <a href="https://golux.lausanne.ch/goeland/affaire2/specialisation/t274/genereate_png_position_arbres.php?npixwidth=300&npixheight=300&symbsize=8&strjson=[<?= $link ?>]">
+                Votre image
+            </a>
+            <hr>
+            <p>Copier/Coller le lien :</p>
+            <div class="alert alert-danger">
+                <span id="link">
+                    https://golux.lausanne.ch/goeland/affaire2/specialisation/t274/genereate_png_position_arbres.php?npixwidth=300&npixheight=300&symbsize=8&strjson=[<?= str_replace("%22", '"', $link) ?>]
+                </span>
+            </div>
         </div>
     </div>
 <?php
